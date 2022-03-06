7b53ebc30e3e6296c21d2f3957457994b488e5d8
AINOI <ltccb30274@gmail.com>
2022-03-06 19:09:06 +0800

    upload profile 0306

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+const scroll = ref(false)
+window.addEventListener('scroll', () => {
+  scroll.value = (window.scrollY > 0)
+} )
+
+</script>
+
+<template lang="pug">
+#section.section1(:class="{scroll}")
+  img.minebg(src="./assets/room.png")
+  .circleGroup
+    .circle
+    .circle
+    .circle
+    .circle
+    .circle
+    .circle
+#section.section2
+  
+</template>
+
+<style lang="scss">
+@import './scss/style.scss';
+
+.scroll .kvList {
+  width: 1000px;
+  height: 300px;
+}
+</style>
