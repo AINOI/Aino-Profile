a9b93e255600a2487f1ccd83844d5ce8eb25fa56
AINOI <ltccb30274@gmail.com>
2022-03-07 23:44:28 +0800

    update profile 0307

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,16 @@ import { ref } from 'vue'
 const scroll = ref(false)
 window.addEventListener('scroll', () => {
   scroll.value = (window.scrollY > 0)
-} )
+})
+
+const lastCircle = document.getElementsByClassName('circle:nth-child(6)')
 
 </script>
 
 <template lang="pug">
 #section.section1(:class="{scroll}")
+  .titleBox
+    .titleText This is Aino's Profile.
   img.minebg(src="./assets/room.png")
   .circleGroup
     .circle
@@ -18,14 +22,10 @@ window.addEventListener('scroll', () => {
     .circle
     .circle
 #section.section2
-  
+
 </template>
 
 <style lang="scss">
-@import './scss/style.scss';
+@import './scss/allStyle.scss';
 
-.scroll .kvList {
-  width: 1000px;
-  height: 300px;
-}
 </style>
